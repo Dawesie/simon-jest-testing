3fc1ff69abd53fa6de2f0ae51e7c35fc8c20b78e
Dawesie <marie.dawe01967@gmail.com>
2023-12-04 17:44:23 +0000

    add code for newGame and showScore function.

---
+++ scripts/game.js
@@ -0,0 +1,23 @@
+let game = {
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+    score: 0,
+};
+
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+};
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = {
+    game,
+    newGame,
+    showScore
+};
